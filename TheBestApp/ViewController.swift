6cea69983d85e6d55537e51bb770cc3f217b55bb
Manish Motghare <motgharemm@rknec.edu>
2018-10-08 13:59:26 +0530

    Initial commit

---
+++ TheBestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheBestApp
+//
+//  Created by MANISH MOTGHARE on 08/10/18.
+//  Copyright © 2018 RG-VP WEB SOLUTIONS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
